829cce12ac1da7bdf2f986ce7f8441ef7298f717
Lorenzo Sfarra <lorenzosfarra@gmail.com>
2021-11-17 17:54:13 +0100

    Aligning CmComSmsMessateType constructor parameters order

--- src/Types/CmComSmsMessageType.php
+++ src/Types/CmComSmsMessageType.php
@@ -8,8 +8,17 @@ use NotificationChannels\CmComSmsWhatsApp\Interfaces\SendableMessageInterface;
 /**
  * SMS Message type
  */
-class CmComSmsMessageType extends CmComBaseMessageType implements SendableMessageInterface
+class CmComSmsMessageType extends PlainTextMessageType implements SendableMessageInterface
 {
+
+    public ?string $reference;
+
+    public function __construct(string $from, array $to, string $text, ?string $reference = null)
+    {
+        parent::__construct($from, $to, $text);
+        $this->reference = $reference;
+    }
+
     /**
      * @inheritDoc
      */
--- src/Types/PlainTextMessageType.php
+++ src/Types/PlainTextMessageType.php
@@ -8,19 +8,19 @@ class PlainTextMessageType
     /**
      * @var string
      */
-    public $text;
+    public string $text;
 
     /**
      * @var array List of Recipients
      * @note Twitter requires the snowflake-id
      */
-    public $to = [];
+    public array $to = [];
 
     /**
      * @var string Sender name
      * @note Twitter requires the snowflake-id of the account you want to use as sender
      */
-    public $from;
+    public string $from;
 
     public function __construct(string $from, array $to, string $text)
     {
